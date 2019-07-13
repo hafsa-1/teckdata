757e932e8563dd26c326ed1778a391819fe19e6b
hafsa-1 <50360805+hafsa-1@users.noreply.github.com>
2019-07-13 17:13:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,424 @@
+let resturent = {
+    name: " aghaResturent",
+    area: "gulistan-e-johar",
+    serviceTiming: "10 AM to 3 PM ",
+    homeDelivery: "free",
+    phonNo: "0385453422",
+    emailAdress: "agha12@gmail.com",
+    about: "agha juice shop open since 1998",
+    management: resturentManegmentstaffData,
+    staff: resturentStaffData,
+    menu: {
+        cakes: [],
+        pizzas: [],
+        
+    }
+
+}
+let resturentManegmentstaffData = [
+    {
+        SeniourmanegerName: "nadeemhussian",
+        fathername: "------------",
+        cnic: "****************",
+        phoneNo: "0375756669799",
+        gmail: "************",
+        background: "------------",
+        education: "-------------",
+    },
+    {
+        juniourmanegerName: "rajabali",
+        fathername: "------------",
+        cnic: "****************",
+        phoneNo: "03885999797",
+        gmail: "************",
+        background: "------------",
+        education: "-------------",
+
+    },
+]
+let resturentStaffData = [
+    {
+        nameOfStaff: "sadaf",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "0276644544",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "mehmood",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "03997498",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "saima",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "037454008",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "alihaider",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "03987465123",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "laiba",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "0300948577",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "shahzaib",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "03008556565",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "amna",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "0387764387087",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "komal",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "037750697877",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "usman",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "03003995857",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "rajab",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "039958667",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+    {
+        nameOfStaff: "faria",
+        fathername: "#",
+        cnicNo: "**********",
+        phoneNo: "03887465656",
+        gmail: "****************",
+        background: "-------------",
+        education: "----------",
+    },
+]
+let listOfTotlResturentItems = [
+  "juice", "milkshake", "icecream", "falooda", "coffee", "deserts", "cakes"
+]
+let juiceItmsName = [
+
+    {
+        name: "mangoJuice",
+        smallOneGlass: "RS.50",
+        largeOneGlass: "RS.150",
+
+    },
+    {
+        juiceName: "banana",
+        smallOneGlass: "RS.120",
+        largeOneGlass: "RS.180",
+    },
+    {
+        juiceName: "orange",
+        smallOneGlass: "RS.70",
+        largeOneGlass: "RS.90",
+    },
+    {
+        juiceName: "graps",
+        smallOneGlass: "RS.200",
+        largeOneGlass: "RS.300",
+    },
+    {
+        juiceName: "apple",
+        smallOneGlass: "RS.350",
+        largeOneGlass: "RS.400",
+
+    },
+    {
+        juiceName: "pineapple",
+        smallOneGlass: "RS.500",
+        largeOneGlass: "RS.600",
+    },
+    {
+        juiceName: "gray",
+        smallOneGlass: "RS.600",
+        largeOneGlass: "RS.750",
+    }
+]
+let icecreamItemsNames = [
+    {
+        name: "pista",
+        smallCup: "90",
+        largeCup: "160",
+    },
+    {
+        name: "simple",
+        smallCup: "60",
+        largeCup: "90",
+    },
+
+    {
+        name: "vanila",
+        smallCup: "100",
+        largeCup: "180",
+    },
+    {
+        name: "fruity",
+        smallCup: "60",
+        largeCup: "170",
+    },
+    {
+        name: "trutyfruity",
+        smallCup: "80",
+        largeCup: "180",
+    },
+    {
+        icecreamName: "strawberry",
+        smallCup: "100",
+        largeCup: "180",
+    },
+    {
+        icecreamName: "mango",
+        smallCup: "100",
+        largeCup: "190",
+    },
+    {
+        icecreamName: "chocolait",
+        smallCup: "150",
+        largeCup: "200",
+    },
+]
+let falodaItemsName = [
+    {
+        falodaName: "special-faloda",
+        smallGlass: "180",
+        mediumGlass: "200",
+        largeGlass: "300",
+    },
+    {
+        falodaName: "apple-faloda",
+        smallGlass: "150",
+        mediumGlass: "180",
+        largeGlass: "250",
+    },
+    {
+        falodaName: "pineApple-faloda",
+        smallGlass: "200",
+        mediumGlass: "250",
+        largeGlass: "300",
+    },
+    {
+        falodaName: "simple-faloda",
+        smallGlass: "150",
+        mediumGlass: "170",
+        largeGlass: "200",
+    },
+    {
+        falodaName: "chocolait-faloda",
+        smallGlass: "180",
+        mediumGlass: "250",
+        largeGlass: "300",
+    },
+    {
+        falodaName: "special-double-chocolait-faloda",
+        smallGlass: "250",
+        mediumGlass: "400",
+        largeGlass: "550",
+    },
+    {
+        falodaName: "strawberry-faloda",
+        smallGlass: "200",
+        mediumGlass: "280",
+        largeGlass: "300",
+    },
+]
+let coffeeItemsNames = [
+    {
+        coffeeName: "coldcoffee",
+        smallGlass: "250",
+        mediumGlass: "300",
+        largeGlass: "350",
+    },
+    {
+        coffeeName: "hotcoffee",
+        smallGlass: "250",
+        mediumGlass: "300",
+        largeGlass: "380",
+    },
+]
+let milkShaksItemsNames = [
+    {
+        milkShaksName: "mangomilkshake",
+        smallOneGlass: "200",
+        mediumOneGlass: "250",
+        largeOneGlass: "300",
+    },
+    {
+        milkShaksName: "bananamilkshake",
+        smallOneGlass: "250",
+        mediumOneGlass: "280",
+        largeOneGlass: "300",
+    },
+    {
+        milkShaksName: "strawberrymilkshake",
+        smallOneGlass: "300",
+        mediumOneGlass: "350",
+        largeOneGlass: "400",
+    },
+    {
+        milkShaksName: "mix-fruit-milkshake",
+        smallOneGlass: "300",
+        mediumOneGlass: "400",
+        largeOneGlass: "500",
+    },
+    {
+        milkShaksName: "pineapple-milkshake",
+        smallOneGlass: "600",
+        mediumOneGlass: "700",
+        largeOneGlass: "1000",
+    },
+    {
+        milkShaksName: "neem-milkshake",
+        smallOneGlass: "400",
+        mediumOneGlass: "450",
+        largeOneGlass: "500",
+    },
+    {
+        milkShaksName: "mint-milkshake",
+        smallOneGlass: "360",
+        mediumOneGlass: "450",
+        largeOneGlass: "500",
+    },
+    {
+        milkShaksName: "icecream-milkshake",
+        smallOneGlass: "300",
+        mediumOneGlass: "350",
+        largeOneGlass: "400",
+    },
+    {
+        milkShaksName: "vanila-ice-milkshake",
+        smallOneGlass: "280",
+        mediumOneGlass: "300",
+        largeOneGlass: "380",
+    },
+]
+let desertsItemsNames = [
+    {
+        desertsName: "mixsweets",
+        halfKgPrice: "400",
+        oneKgPrice: "700",
+    },
+    {
+        desertsName: "labesheeri",
+        halfKgPrice: "550",
+        oneKgPrice: "800",
+    },
+    {
+        desertsName: "rasmalai",
+        halfKgPrice: "550",
+        oneKgPrice: "800",
+    },
+    {
+        desertsName: "kheer",
+        halfKgPrice: "500",
+        oneKgPrice: "700",
+    },
+    {
+        desertsName: "chocolait",
+        halfKgPrice: "400",
+        oneKgPrice: "700",
+    },
+]
+let cakesItemsName = [
+    {
+        cakeNames: "pineapple-cake",
+        small: "350",
+        large: "600",
+    },
+    {
+        cakeNames: "vanila-cake",
+        small: "550",
+        large: "800",
+    },
+    {
+        cakeNames: "chocolait-cake",
+        small: "700",
+        large: "1000",
+    },
+    {
+        cakeNames: "chocolait-double-lyers-cake",
+        small: "1000",
+        large: "1500",
+    },
+    {
+        cakeNames: "black-forest-geteua-cake",
+        small: "1000",
+        large: "1600",
+    },
+    {
+        cakeNames: "chiffon-cake",
+        small: "900",
+        large: "1200",
+    },
+    {
+        cakeNames: "carrot-cake",
+        small: "1050",
+        large: "1300",
+    },
+    {
+        cakeNames: "gooue-butter-cake",
+        small: "1500",
+        large: "2000",
+    },
+    {
+        cakeNames: "coffee-cake",
+        small: "1400",
+        large: "2500",
+    },
+    {
+        cakeNames: "german-chocolait-cake",
+        small: "2000",
+        large: "3500",
+    },
+    {
+        cakeNames: "princess-cake",
+        small: "1500",
+        large: "3000",
+    },
+
+]
